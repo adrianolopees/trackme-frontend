047dc2d546e4389e3c7c7f85cd1552102c553218
adrianolopees <adrianodeev1@gmail.com>
2025-07-22 11:47:18 -0300

    tailwind config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import {
   UserGreeting,
   AuthButtons,
 } from "../components";
+import { FiShare2, FiUsers, FiUser, FiMusic } from "react-icons/fi";
 
 function Home() {
   const { isAuthenticated, profile, loading } = useAuth();
@@ -20,14 +21,29 @@ function Home() {
         <AuthButtons />
       )}
 
-      <div className="mt-12 text-center max-w-2xl">
-        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+      <div className="mt-22 text-center max-w-2xl">
+        <h2 className="text-2xl font-semibold text- mb-8 text-gray-800">
           O que é o TrackMe?
         </h2>
 
-        <p className="text-gray-600 leading-relaxed">
-          Transforme playlists em conexões.”
-        </p>
+        <ul className="space-y-3 text-gray-600 leading-relaxed">
+          <li className="flex items-start gap-3">
+            <FiShare2 className="text-primary mt-1" size={18} />
+            <span>Transforme playlists em conexões.</span>
+          </li>
+          <li className="flex items-start gap-3">
+            <FiUsers className="text-primary mt-1" size={18} />
+            <span>Sua vibe atrai sua tribo.</span>
+          </li>
+          <li className="flex items-start gap-3">
+            <FiMusic className="text-primary mt-1" size={18} />
+            <span>Mesma música, nova amizade.</span>
+          </li>
+          <li className="flex items-start gap-3">
+            <FiUser className="text-primary mt-1" size={18} />
+            <span>Seu perfil, suas regras, sua essência.</span>
+          </li>
+        </ul>
       </div>
     </PageWrapper>
   );
