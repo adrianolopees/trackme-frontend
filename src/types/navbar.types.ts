89a71cf77bff76408f6f6e8a1408358f663c8454
adrianolopees <adrianodeev1@gmail.com>
2025-09-05 10:47:49 -0300

    refactor: movi os types da navbar para a pasta types reaproveitando SafeProfile para tipar corretamente profile .

---
+++ src/types/navbar.types.ts
@@ -0,0 +1,13 @@
+import type { SafeProfile } from "../schemas/profileSchemas";
+export interface MenuItem {
+  label: string;
+  href: string;
+}
+export interface NavbarProps {
+  isAuthenticated?: boolean;
+  loading?: boolean;
+  profile?: SafeProfile;
+  brandName?: string;
+  showMenuItems?: boolean;
+  menuItems?: MenuItem[];
+}
