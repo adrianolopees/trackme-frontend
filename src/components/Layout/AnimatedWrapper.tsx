a6955c31225dd9907c2e201f79d4eee2fb7ba7ad
adrianolopees <adrianodeev1@gmail.com>
2025-09-24 10:37:45 -0300

    refactor:adicionando animaçao de mudança entre forms no modal login e regsiter.

--- src/components/Layout/AnimatedWrapper.tsx
+++ src/components/Layout/AnimatedWrapper.tsx
@@ -12,11 +12,11 @@ export default function AnimatedWrapper({
 }: AnimatedWrapperProps) {
   return (
     <motion.div
-      className={className} // Deixa o controle total das classes para o usuário
-      initial={{ opacity: 0, y: 20 }} // Animação de entrada (de baixo para cima com fade in)
-      animate={{ opacity: 1, y: 0 }} // Estado final (visível e posicionado)
-      exit={{ opacity: 0, y: -20 }} // Animação de saída (para cima com fade out)
-      transition={{ duration: 0.5 }} // Duração da transição (pode customizar via props se quiser mais flexibilidade)
+      className={className}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.5 }}
     >
       {children}
     </motion.div>
